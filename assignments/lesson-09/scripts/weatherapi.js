f6208ae291ea2e03e195c8f2f6835fcce8198ae5
Joseph Wurz <wur17002@byui.edu>
2018-11-16 18:00:48 -0600

    open weather map

--- assignments/lesson-09/scripts/weatherapi.js
+++ assignments/lesson-09/scripts/weatherapi.js
@@ -1,2 +1,10 @@
-let weatherRequest = new XMLHttpRequest();
-let requestURL = 'https://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=92a74429686969d072af8620a219e47d';
+let weatherObject = new XMLHttpRequest();
+weatherObject.open('GET', 'https:api.openweathermap.org/data/2.5/weather?id=4691930&APPID=92a74429686969d072af8620a219e47d&units=imperial', true);
+
+weatherObject.send();
+
+weatherObject.onload = function() {
+    let weatherInfo = JSON.parse(weatherObject.responseText);
+    console.log(weatherInfo);
+    document.getElementById('current-temp').innerHTML = weatherInfo.main.temp;
+}
