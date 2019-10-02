0bef408ecc1e2c3445dd027f338aea65f36d9b30
Joseph Wurz <wur17002@byui.edu>
2019-10-02 17:16:40 -0500

    update to json.js

--- cit261/week03/json.js
+++ cit261/week03/json.js
@@ -1,16 +1,12 @@
 //JSON format
-var jsonData = '{"fName": "Joseph", "middleInitial": "null", "lName": "Smith", "order": 1, "birthState": "Vermont"}';
-//JavaScript Object format
-var jsObject = { fName: "Joseph", middleInitial: null, lName: "Smith", order: 1, birthState: "Vermont" };
+var jsonData = '{"fName": "Joseph", "middleInitial": null, "lName": "Smith", "order": 1, "birthState": "Vermont"}';
 
 //use parse to convert from JSON to JavaScript object.
 var obj = JSON.parse(jsonData);
 
-console.log(jsonData);
-console.log(jsObject);
+console.log(obj);
 
-
-//document.write("<br>" + obj.fName + " " + obj.lName + " " + "was born in " + obj.birthState);
+document.write("<br>" + obj.fName + " " + obj.lName + " " + "was born in " + obj.birthState);
 
 
 //use stringify to convert from JavaScript object to JSON format.
