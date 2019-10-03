5d386415720faac54a6d5044f25867bb5966ffa9
Joseph Wurz <wur17002@byui.edu>
2019-10-03 13:55:28 -0500

    events

---
+++ cit261/week03/scripts/events.js
@@ -0,0 +1,32 @@
+function alertOnLoad() {
+    alert("This was triggered by the onload event handler");
+}
+//show temple image
+function displayImage() {
+    document.getElementById("image").style.display = "block";
+}
+//make image bigger/smaller
+var getImage = document.getElementById("image");
+getImage.addEventListener("mouseover", enlarge);
+getImage.addEventListener("mouseleave", shrink);
+
+function enlarge() {
+    getImage.style.transform = "scale(1.3)";
+}
+function shrink() {
+    getImage.style.transform = "scale(1)";
+}
+
+//animate the square
+function moveSquare() {
+    document.getElementById("square").animate([
+        { transform: 'translate(0, 0)' },
+        { transform: 'translate(280px, 478px)' },
+        { transform: 'translate(0, 0)' }
+
+    ], {
+        duration: 6000,
+        iterations: Infinity
+    })
+}
+document.getElementById("square").addEventListener("touchstart", moveSquare);
