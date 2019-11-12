7edaa1cd3ff796b4b51cf4c01b762f3253e9da90
Joseph Wurz <wur17002@byui.edu>
2019-11-11 18:03:59 -0600

    css with javascript

---
+++ cit261/week09/css_javascript.js
@@ -0,0 +1,11 @@
+//div one
+document.querySelector(".one").style.color = "blue";
+document.querySelector(".one").style.backgroundColor = "gray";
+
+//div two
+document.querySelector(".two").style.cssText = "color: white; background-color: black; border-radius: 15px;";
+
+//div three
+btn.onclick = function () {
+    document.querySelector(".three").style.cssText = "color: white; background-color: #4c4c4c; width: 50px; border-radius: 50%;"
+}
